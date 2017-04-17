cb1833e533e820f46b904a52fa0c98754a250edc
Sophie Deziel <courrier@sophiedeziel.com>
2017-04-16 21:19:33 -0400

    First test + implementation. Must fix devise dependency.

--- lib/has_prerequisite.rb
+++ lib/has_prerequisite.rb
@@ -1,5 +1,56 @@
 require "has_prerequisite/version"
+require "active_support"
 
 module HasPrerequisite
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :prerequisites, :skipping_checks
+    self.prerequisites = []
+    self.skipping_checks = false
+
+    rescue_from HasPrerequisite::PrerequisiteNotMet, with: :prerequisite_not_met!
+  end
+
+  module ClassMethods
+    def prerequisite(method, redirection_path: nil, **options)
+      before_action :perform_checks
+
+      prerequisites << { method: method, redirection_path: redirection_path, options: options }
+    end
+
+    def fulfilling_prerequisite
+      self.skipping_checks = true
+    end
+  end
+
+  def step_fulfilled!
+    redirect_to stored_location_for(:user)
+  end
+
+  private
+
+  def perform_checks
+    return if self.class.skipping_checks
+    return unless failing_preriquisite
+    store_location_for(:user, request.fullpath)
+    raise HasPrerequisite::PrerequisiteNotMet
+  end
+
+  def failing_preriquisite
+    @failing_preriquisite ||= prerequisites.find do |p|
+      !send(p[:method]) if p[:options][:if].nil? || send(p[:options][:if])
+    end
+  end
+
+  def prerequisite_not_met!
+    redirect_to redirect_path
+  end
+
+  def redirect_path
+    return send(failing_preriquisite[:redirection_path]) if failing_preriquisite[:redirection_path].is_a? Symbol
+    failing_preriquisite[:redirection_path]
+  end
+
+  class PrerequisiteNotMet < StandardError; end
 end
--- spec/has_prerequisite_spec.rb
+++ spec/has_prerequisite_spec.rb
@@ -1,11 +1,87 @@
 require 'spec_helper'
+require_relative 'dummy/spec/rails_helper'
 
-describe HasPrerequisite do
+describe HasPrerequisite::VERSION do
+  it 'has a version number' do
+    expect(HasPrerequisite::VERSION).not_to be nil
+  end
+end
+
+describe HasPrerequisite, type: :controller do
   it 'has a version number' do
     expect(HasPrerequisite::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  controller(ApplicationController) do
+      include HasPrerequisite
+
+      prerequisite :prerequisite1, redirection_path: 'path_1'
+      prerequisite :prerequisite2, redirection_path: 'path_2', if: :condition
+
+      before_action :perform_checks
+
+      def index
+        render text: nil
+      end
+
+      private
+
+      def prerequisite1
+        true
+      end
+
+      def prerequisite2
+        true
+      end
+
+      def condition
+        true
+      end
   end
+
+  before { @controller = controller }
+  subject { get :index }
+
+  describe 'redirection' do
+    it 'does not interfere when the prerequisite is met' do
+      expect(controller).to receive(:prerequisite1) { true }
+      expect(controller).to receive(:prerequisite2) { true }
+      expect(subject).to be_success
+    end
+
+    it 'it redirects to the path when the prerequisite is not met' do
+      expect(controller).to receive(:prerequisite1) { true }
+      expect(controller).to receive(:prerequisite2) { false }
+      expect(subject).to redirect_to 'path_2'
+    end
+  end
+
+  describe 'conditionnal' do
+    it 'it supports an `if` option to skip the check' do
+      expect(controller).to receive(:condition) { false }
+      expect(controller).to_not receive(:prerequisite2)
+      expect(subject).to be_success
+    end
+  end
+
+  context 'is a controller that skips the prerequisites' do
+    controller(ApplicationController) do
+      include HasPrerequisite
+
+      prerequisite :pre, redirection_path: 'path_1'
+      fulfilling_prerequisite
+
+      def index
+        render text: nil
+      end
+    end
+
+    describe 'fulfilling_prerequisite' do
+      it 'skips the checks' do
+        expect(controller).to_not receive(:pre)
+        expect(subject).to be_success
+      end
+    end
+  end
+
 end

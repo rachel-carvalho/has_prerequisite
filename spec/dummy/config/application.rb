c185d20c0cf6fdb32263c2cb6388ea5b0468d140
Sophie Deziel <courrier@sophiedeziel.com>
2017-04-16 21:18:13 -0400

    regenerate the dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,19 +1,19 @@
 require_relative 'boot'
 
 # Pick the frameworks you want:
-# require "active_record/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 require "active_job/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "has_prerequisite"
 
-module TestApp
+module Dummy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
